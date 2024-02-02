7da25618e4ecbe7d4bcde23f785834f5bca2810e
suzakuwcx <suzakuwcx@gmail.com>
2024-02-02 10:39:09 +0800

    FunctionBus/PlayerStopUsingItemEventBus.java: Use '_instanceof' instead of type check for 'isPlayerStopUsingRifle'

--- src/main/java/FunctionBus/PlayerStopUsingItemEventBus.java
+++ src/main/java/FunctionBus/PlayerStopUsingItemEventBus.java
@@ -1,13 +1,12 @@
 package FunctionBus;
 
-import org.bukkit.Material;
-
+import Assert.Item.Gun.Rifle;
 import Task.GunTask.RifleTask;
 import io.papermc.paper.event.player.PlayerStopUsingItemEvent;
 
 public class PlayerStopUsingItemEventBus {
     public static boolean isPlayerStopUsingRifle(PlayerStopUsingItemEvent event) {
-        if (event.getPlayer().getInventory().getItemInMainHand().getType() != Material.CROSSBOW)
+        if (!Rifle._instanceof(event.getPlayer().getInventory().getItemInMainHand()))
             return false;
         return true;
     }
