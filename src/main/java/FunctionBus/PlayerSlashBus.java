2ff4d32ea41eb2d9864a3396269454dee4ae29ce
suzakuwcx <suzakuwcx@gmail.com>
2024-04-06 02:55:45 +0800

    Player can use sword to rebound arrows

--- src/main/java/FunctionBus/PlayerSlashBus.java
+++ src/main/java/FunctionBus/PlayerSlashBus.java
@@ -1,9 +1,14 @@
 package FunctionBus;
 
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
 public class PlayerSlashBus {
     public static void onPlayerSlash(Player player) {
-        
+        for (Entity e: ServerBus.getNearbyEntities(player.getEyeLocation(), 4, 2, 4, EntityType.ARROW)) {
+            if (PlayerBus.isEntityInFrontOfPlayer(player, e, 4, Math.PI / 4))
+            e.setVelocity(e.getVelocity().multiply(-1.5));
+        }
     }
 }
