855fa2493c65649254a336b29ef21acb19c2b839
suzakuwcx <suzakuwcx@gmail.com>
2024-04-06 23:06:48 +0800

    Update remaining actions

--- src/main/java/Task/SwipeTask/KatateMigiKiriageTask.java
+++ src/main/java/Task/SwipeTask/KatateMigiKiriageTask.java
@@ -1,26 +1,29 @@
 package Task.SwipeTask;
 
-import static java.lang.Math.sqrt;
-
 import java.util.HashMap;
 import java.util.Map;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Particle;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
 import FunctionBus.ServerBus;
-import Schedule.PlayerRealVelocityUpdateSchedule;
 
-public class KatateMigiKiriageTask implements Runnable{
+/* 
+ *  particle minecraft:wax_off ~ ~2 ~ 0 0 0 0 1 
+ */
+public class KatateMigiKiriageTask implements Runnable {
     private static Map<Player, KatateMigiKiriageTask> task_mapper;
-    private static final int MAX_TICK = 6;
+    private static final int MAX_TICK = 4;
+    private static final int MAX_FRAME = 8;
 
     private Player player;
     private Location location;
-    private Vector vec = new Vector(2, -1, 0).rotateAroundAxis(new Vector(1, 2 * sqrt(2), -1), Math.PI / 6);
+    private Vector axis = new Vector(0, 1, 0).rotateAroundZ(Math.PI / 10).normalize();
+    private Vector vec = (new Vector(0, 0, 1).crossProduct(axis)).normalize().multiply(4).rotateAroundAxis(axis, - Math.PI / 6);
     private double rotation = 4 * Math.PI / 3;
     private int tick = MAX_TICK;
 
@@ -37,7 +40,9 @@ public class KatateMigiKiriageTask implements Runnable{
         task.player = player;
         task.location = player.getEyeLocation();
         /* delay compensation */
-        task.location.add(PlayerRealVelocityUpdateSchedule.getVelocity(player).clone().setY(0).multiply(9));
+        // task.location.add(PlayerRealVelocityUpdateSchedule.getVelocity(player).clone().setY(0).multiply(9));
+
+        ServerBus.playServerSound(task.location, Sound.ENTITY_PLAYER_ATTACK_SWEEP, 1f, 1.5f);
 
         Bukkit.getScheduler().runTask(ServerBus.getPlugin(), task);
     }
@@ -45,15 +50,11 @@ public class KatateMigiKiriageTask implements Runnable{
     @Override
     public void run() {
         if (tick > 0) {
-            ServerBus.spawnServerParticle(Particle.WAX_OFF, ServerBus.toLocalCoordinates(location, vec), 1, 0, 0, 0, 0);
-            vec.rotateAroundAxis(new Vector(1, 2 * sqrt(2), -1), - rotation / MAX_TICK / 4);
-            ServerBus.spawnServerParticle(Particle.WAX_OFF, ServerBus.toLocalCoordinates(location, vec), 1, 0, 0, 0, 0);
-            vec.rotateAroundAxis(new Vector(1, 2 * sqrt(2), -1), - rotation / MAX_TICK / 4);
-            ServerBus.spawnServerParticle(Particle.WAX_OFF, ServerBus.toLocalCoordinates(location, vec), 1, 0, 0, 0, 0);
-            vec.rotateAroundAxis(new Vector(1, 2 * sqrt(2), -1), - rotation / MAX_TICK / 4);
-            ServerBus.spawnServerParticle(Particle.WAX_OFF, ServerBus.toLocalCoordinates(location, vec), 1, 0, 0, 0, 0);
+            for (int i = 0; i < MAX_FRAME; ++i) {
+                ServerBus.spawnServerParticle(Particle.WAX_OFF, ServerBus.toLocalCoordinates(location, vec), 1, 0, 0, 0, 0);
+                vec.rotateAroundAxis(axis, rotation / MAX_TICK / MAX_FRAME);
+            }
             --tick;
-            vec.rotateAroundAxis(new Vector(1, 2 * sqrt(2), -1), - rotation / MAX_TICK / 4);
             Bukkit.getScheduler().runTaskLater(ServerBus.getPlugin(), this, 1);
         } else {
             task_mapper.remove(player);
--- src/main/java/Task/SwipeTask/YokogiriTask.java
+++ src/main/java/Task/SwipeTask/YokogiriTask.java
@@ -6,23 +6,25 @@ import java.util.Map;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Particle;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
 import FunctionBus.ServerBus;
-import Schedule.PlayerRealVelocityUpdateSchedule;
 
 /* 
  *  particle minecraft:wax_off ~ ~2 ~ 0 0 0 0 1 
  */
 public class YokogiriTask implements Runnable {
     private static Map<Player, YokogiriTask> task_mapper;
-    private static final int MAX_TICK = 6;
+    private static final int MAX_TICK = 4;
+    private static final int MAX_FRAME = 8;
 
     private Player player;
     private Location location;
-    private Vector vec = new Vector(1.5, 0, 0).rotateAroundY(Math.PI / 6);
-    private double rotation = 4 * Math.PI / 3;
+    private Vector axis = new Vector(0, 1, 0).rotateAroundZ(0).normalize();
+    private Vector vec = (new Vector(0, 0, 1).crossProduct(axis)).normalize().multiply(-4).rotateAroundAxis(axis, Math.PI / 6);
+    private double rotation = - 4 * Math.PI / 3;
     private int tick = MAX_TICK;
 
     static {
@@ -34,14 +36,13 @@ public class YokogiriTask implements Runnable {
             return;
 
         YokogiriTask task = new YokogiriTask();
-        Vector direction = player.getEyeLocation().getDirection().setY(0).normalize();
         task_mapper.put(player, task);
         task.player = player;
         task.location = player.getEyeLocation();
         /* delay compensation */
-        task.location.add(PlayerRealVelocityUpdateSchedule.getVelocity(player).clone().setY(0).multiply(9));
-        /* rotate center is forward 0.3 block for more smooth looking */
-        task.location.add(direction.multiply(0.3));
+        // task.location.add(PlayerRealVelocityUpdateSchedule.getVelocity(player).clone().setY(0).multiply(9));
+
+        ServerBus.playServerSound(task.location, Sound.ENTITY_PLAYER_ATTACK_SWEEP, 1f, 1.5f);
 
         Bukkit.getScheduler().runTask(ServerBus.getPlugin(), task);
     }
@@ -49,15 +50,11 @@ public class YokogiriTask implements Runnable {
     @Override
     public void run() {
         if (tick > 0) {
-            ServerBus.spawnServerParticle(Particle.WAX_OFF, ServerBus.toLocalCoordinates(location, vec), 1, 0, 0, 0, 0);
-            vec.rotateAroundY(- rotation / MAX_TICK / 4);
-            ServerBus.spawnServerParticle(Particle.WAX_OFF, ServerBus.toLocalCoordinates(location, vec), 1, 0, 0, 0, 0);
-            vec.rotateAroundY(- rotation / MAX_TICK / 4);
-            ServerBus.spawnServerParticle(Particle.WAX_OFF, ServerBus.toLocalCoordinates(location, vec), 1, 0, 0, 0, 0);
-            vec.rotateAroundY(- rotation / MAX_TICK / 4);
-            ServerBus.spawnServerParticle(Particle.WAX_OFF, ServerBus.toLocalCoordinates(location, vec), 1, 0, 0, 0, 0);
+            for (int i = 0; i < MAX_FRAME; ++i) {
+                ServerBus.spawnServerParticle(Particle.WAX_OFF, ServerBus.toLocalCoordinates(location, vec), 1, 0, 0, 0, 0);
+                vec.rotateAroundAxis(axis, rotation / MAX_TICK / MAX_FRAME);
+            }
             --tick;
-            vec.rotateAroundY(- rotation / MAX_TICK / 4);
             Bukkit.getScheduler().runTaskLater(ServerBus.getPlugin(), this, 1);
         } else {
             task_mapper.remove(player);
