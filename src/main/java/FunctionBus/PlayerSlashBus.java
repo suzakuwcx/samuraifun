23491d99790f96194d931cd24bab4ff44691b3a3
suzakuwcx <suzakuwcx@gmail.com>
2024-04-11 00:19:40 +0800

    FunctionBus/PlayerSlashBus.java: Support range modification by attack cooldown

--- src/main/java/FunctionBus/PlayerSlashBus.java
+++ src/main/java/FunctionBus/PlayerSlashBus.java
@@ -5,20 +5,23 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Zombie;
 
+import Assert.Config.PlayerConfig;
 import DataBus.PlayerDataBus;
 
 public class PlayerSlashBus {
     public static void onPlayerSlash(Player player) {
-        for (Zombie e: ServerBus.getNearbyEntities(player.getEyeLocation(), 4, 2, 4, EntityType.ZOMBIE, Zombie.class)) {
-            if (!PlayerBus.isEntityInFrontOfPlayer(player, e, 4, 4 * Math.PI / 3))
+        double range = PlayerConfig.BASIC_ATTACK_RANGE * player.getAttackCooldown();
+
+        for (Zombie e: ServerBus.getNearbyEntities(player.getEyeLocation(), range, 2, range, EntityType.ZOMBIE, Zombie.class)) {
+            if (!PlayerBus.isEntityInFrontOfPlayer(player, e, range, 4 * Math.PI / 3))
                 continue;
             
             PlayerDataBus.downPlayerSlash(player);
             player.attack(e);
         }
 
-        for (Arrow e: ServerBus.getNearbyEntities(player.getEyeLocation(), 4, 2, 4, EntityType.ARROW, Arrow.class)) {
-            if (PlayerBus.isEntityInFrontOfPlayer(player, e, 4, Math.PI / 3))
+        for (Arrow e: ServerBus.getNearbyEntities(player.getEyeLocation(), range, 2, range, EntityType.ARROW, Arrow.class)) {
+            if (PlayerBus.isEntityInFrontOfPlayer(player, e, range, Math.PI / 3))
                 e.setVelocity(e.getVelocity().multiply(-1.5));
         }
     }
