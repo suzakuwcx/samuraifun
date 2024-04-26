6aef170e518fe01f99d2af47d2eb8aed087d98b7
suzakuwcx <suzakuwcx@gmail.com>
2024-04-26 23:45:15 +0800

    ItemDisplayAnimationTask: fix spell mistake from 'SpwanEntity' to 'SpawnEntity'

--- src/main/java/Task/ModelTask/ItemDisplayAnimationTask.java
+++ src/main/java/Task/ModelTask/ItemDisplayAnimationTask.java
@@ -3,13 +3,13 @@ package Task.ModelTask;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.ItemDisplay;
 
-import Assert.Entity.SpwanEntity;
+import Assert.Entity.SpawnEntity;
 import FunctionBus.ServerBus;
 
 public class ItemDisplayAnimationTask implements Runnable {
     private ItemDisplay display;
 
-    public static void execute(SpwanEntity<ItemDisplay> entity, int tick) {
+    public static void execute(SpawnEntity<ItemDisplay> entity, int tick) {
         entity.spwan();
         ItemDisplayAnimationTask task = new ItemDisplayAnimationTask(entity.getEntity());
         Bukkit.getScheduler().runTaskLater(ServerBus.getPlugin(), task, tick);
