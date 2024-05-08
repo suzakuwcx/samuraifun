3cd1b1c49bac752ac28d66fe4bde706523837418
suzakuwcx <suzakuwcx@gmail.com>
2024-05-08 23:15:00 +0800

    Add basic implement for player posture crash status

---
+++ src/main/java/Task/StateTask/PlayerPostureCrashTask.java
@@ -0,0 +1,54 @@
+package Task.StateTask;
+
+import org.bukkit.Color;
+import org.bukkit.Material;
+import org.bukkit.Particle.DustOptions;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import Assert.Config.PlayerConfig;
+import Assert.Item.Sword;
+import FunctionBus.PlayerBus;
+import FunctionBus.ServerBus;
+import Schedule.PlayerStateMachineSchedule;
+
+
+public class PlayerPostureCrashTask extends BaseStateTask {
+    private Player player;
+    private int tick = 0;
+
+    public PlayerPostureCrashTask(Player player) {
+        this.player = player;
+
+        player.setCooldown(Material.SHIELD, 500000);
+        player.completeUsingActiveItem();
+    }
+
+    @Override
+    public void onPlayerInteractEvent(PlayerInteractEvent event) {
+        event.setCancelled(true);
+    }
+
+    @Override
+    public void onPlayerToggleSneakEvent(PlayerToggleSneakEvent event) {
+        return;
+    }
+
+    @Override
+    public void run() {
+        if (tick == 30) {
+            PlayerBus.setPlayerInventoryList(player, new Sword(1003), 0, 3, 6);
+            PlayerStateMachineSchedule.setStateTask(player, new BattleStateTask(player));
+            PlayerStateMachineSchedule.getPlayerState(player).posture = PlayerConfig.MAX_POSTURE;
+        }
+        
+        PlayerBus.banPlayerJump(player, 3);
+        player.addPotionEffect(new PotionEffect(PotionEffectType.DARKNESS, 3, 1, false, false));
+        player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 3, 5, false, false));
+        ServerBus.spawnServerDustParticle(player.getLocation(), 3, 1, 1, 1, new DustOptions(Color.YELLOW, 3));
+        ++tick;
+    }
+}
