362dd5adfbc8b42655c3ef00e55390fd356affc5
suzakuwcx <suzakuwcx@gmail.com>
2024-05-30 00:00:32 +0800

    Add effect for smoking dart that can reset player sword cooldown

--- src/main/java/Task/AttackTask/SmokingDartsTask.java
+++ src/main/java/Task/AttackTask/SmokingDartsTask.java
@@ -5,11 +5,15 @@ import org.bukkit.Color;
 import org.bukkit.FluidCollisionMode;
 import org.bukkit.Location;
 import org.bukkit.Particle.DustOptions;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.util.RayTraceResult;
 import org.bukkit.util.Vector;
 
+import Assert.Config.State;
 import Assert.Entity.SmokingDartsEntity;
 import FunctionBus.ServerBus;
+import Schedule.PlayerStateMachineSchedule;
 
 public class SmokingDartsTask implements Runnable {
     private static final Vector scale = new Vector(3, 3, 3);
@@ -59,6 +63,15 @@ public class SmokingDartsTask implements Runnable {
         if (delay_tick < 0) {
             --tick;
             ServerBus.spawnServerDustParticle(location, 15, 2, 2, 2, new DustOptions(Color.WHITE, 75.0f));
+
+            if (tick % 80 == 0) {
+                for (Player player : ServerBus.getNearbyEntities(location, 5, 5, 5, EntityType.PLAYER, Player.class)) {
+                    if (player.getLocation().distance(location) <= 5) {
+                        State state = PlayerStateMachineSchedule.getPlayerState(player);
+                        state.sword_cooldown = 0;
+                    }
+                }
+            }
         }
 
         
